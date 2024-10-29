e855562b891bc0cd2fc9d9074f4f8da32b31511f
Natan Guimarães <107555990+nacaxi@users.noreply.github.com>
2024-10-29 07:48:22 -0300

    a

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ async function visualizarDados(){
 
   const paragrafo = document.createElement('p')
   paragrafo.classList.add("graficos--container__texto")
-  paragrafo.innerHTML = "No ano de ${ano[0]} foi obtido um valor de ${valor[0]}"
+  paragrafo.innerHTML = `No ano de ${ano[0]} foi obtido um valor de ${valor[0]}`
   console.log(paragrafo)
 
   const container = document.getElementById('graficos--container')
